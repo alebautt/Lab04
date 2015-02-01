2acbe42e9960cfb4d1fd103f087e937482b19631
alebautt <id_bautista@hotmail.com>
2015-02-01 12:22:04 -0600

    Initial Commit

---
+++ Lab04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab04
+//
+//  Created by Alejandra B on 01/02/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
